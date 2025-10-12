69ffb90d15c4a098c42ed6ff4946861fddc3a2e6
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-10-12 16:20:05 +1100

    Add files via upload

---
+++ Proto1 innovation/index.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("ARDENTIFY")
+st.image("c:/Users/ricew/OneDrive/Documents/legend-of-bob/Assets/logo.png", caption=None, width= 300, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.subheader("Live Photo")
+st.write("Take a live photo of " \
+"artifact")
+st.subheader("Upload live Photo of " \
+"artifact")
+st.write("Please upload a photo of the " \
+"artifact.")
+
+st.sidebar.success("scan type")
+
---
+++ Proto1 innovation/pages/About us.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("About Us")
+st.subheader("Learn More About Us")
+st.write("We are AI3 institute a small team of passionate individuals dedicated to creating" \
+" innovative AI solutions that make a positive impact on society." \
+" Our mission is to harness the power of artificial intelligence" \
+    "to help make the lives of archeologist better.")
+st.image("c:/Users/ricew/OneDrive/Documents/legend-of-bob/Assets/logo.png", caption=None, width= 325, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
