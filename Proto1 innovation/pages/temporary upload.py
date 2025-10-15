39d9e706450f2fd75a5e7965298366bab4dfed3a
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-10-15 17:22:19 +1100

    Update temporary upload.py

--- Proto1 innovation/pages/temporary upload.py
+++ Proto1 innovation/pages/temporary upload.py
@@ -4,7 +4,7 @@ import pickle
 
 def start():
     st.title("ARDENTIFY")
-    st.image("c:/Users/ricew/OneDrive/Documents/legend-of-bob/Assets/logo.png", caption=None, width=200, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+    st.image("Proto1 innovation/institute logo.png", caption=None, width=200, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
     st.subheader("Live Photo")
     st.write("Take a live photo of artifact")
     st.write("""
@@ -24,3 +24,4 @@ def main():
 
 
 main()
+
