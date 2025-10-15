73a1ced61ebd069e34299d91a6cde50825dbc247
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-10-15 17:21:44 +1100

    Update About us.py

--- Proto1 innovation/pages/About us.py
+++ Proto1 innovation/pages/About us.py
@@ -6,4 +6,5 @@ st.write("We are AI3 institute a small team of passionate individuals dedicated 
 " innovative AI solutions that make a positive impact on society." \
 " Our mission is to harness the power of artificial intelligence" \
     "to help make the lives of archeologist better.")
-st.image("c:/Users/ricew/OneDrive/Documents/legend-of-bob/Assets/logo.png", caption=None, width= 325, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+
+st.image("Proto1 innovation/institute logo.pngg", caption=None, width= 325, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
