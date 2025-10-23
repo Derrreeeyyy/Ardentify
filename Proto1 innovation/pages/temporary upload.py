fa75f88c8e5b9bff225d48cb54d2195e23ddbb3c
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-10-23 20:15:54 +1100

    Enhance temporary upload.py with user input field
    Added text area for user input after object detection.

--- Proto1 innovation/pages/temporary upload.py
+++ Proto1 innovation/pages/temporary upload.py
@@ -7,14 +7,16 @@ def start():
     st.image("Proto1 innovation/institute logo.png", caption=None, width=200, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
     st.subheader("Live Photo")
     st.write("Take a live photo of artifact")
-    st.write("""
-
-    """)
+    st.write("")
+    st.write("")
+    st.write("")
     if st.button("start camera"):
         st.write("Camera started")
         photo = st.camera_input("Take a photo")
         if st.button("Detect Objects"):
-           st.write("cool")
+           st.text_area("Name,Email", value="", height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", width="stretch")
+
+
 
 
 def main():
@@ -24,4 +26,5 @@ def main():
 
 
 main()
+
 
