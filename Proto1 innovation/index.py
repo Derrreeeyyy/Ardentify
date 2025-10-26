18a678da9531a49674e06d6642fed54b70c5909c
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-10-26 18:55:33 +1100

    Update index.py

--- Proto1 innovation/index.py
+++ Proto1 innovation/index.py
@@ -2,14 +2,12 @@ import streamlit as st
 
 st.title("ARDENTIFY")
 st.image("Proto1 innovation/institute logo.png", caption=None, width= 300, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
-st.subheader("Live Photo")
-st.write("Take a live photo of " \
-"artifact")
 st.subheader("Upload live Photo of " \
 "artifact")
 st.write("Please upload a photo of the " \
 "artifact.")
 
 st.sidebar.success("scan type")
+
 
 
