86135b643c829bc6114c499ff1bc7b58bd05f6c2
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-11-23 19:42:34 +1100

    Implement terms and conditions in Terms_and_Conditions.py
    Added terms and conditions for app usage.

---
+++ Proto1 innovation/pages/Terms_and_Conditions.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Terms and Conditions")
+st.write("By using this app, you agree to these terms and conditions: ")
+st.write("I will not copy any other archeologist’s work and claim it as my own")
+st.write("I will be respectful and stay on task when communicating with others ")
+st.write("I will not spam or grief any post")
+st.write("By abiding by these conditions, you make this app a respectful yet productive place to be in for all archeologists.")
