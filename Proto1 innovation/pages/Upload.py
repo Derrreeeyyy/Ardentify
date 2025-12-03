800430d86297700d605648a559c15e973a67fac0
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-12-03 18:20:36 +1100

    Refactor Upload.py for improved structure

--- Proto1 innovation/pages/Upload.py
+++ Proto1 innovation/pages/Upload.py
@@ -14,14 +14,14 @@ def main():
     image = Image.open(File)
     st.image(image, caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
 
- submit =  st.button("continue submission", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
- if submit:
   st.text_input("Name", value="", max_chars=45, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
   st.text_input("Primary contact details", value="", max_chars=45, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
   st.text_input("Secondary contact details", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
   st.text_input("Country and address of digsite/find", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
   st.text_input("Company/organisation", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
-  
+  submit = st.button("Submit")
+
 
 main()
+
 
