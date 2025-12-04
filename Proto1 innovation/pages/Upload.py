727a0376586c66c63372777af05dab023f45d6da
Derrreeeyyy <derreeeyyyy@gmail.com>
2025-12-04 20:10:31 +1100

    Update Upload.py

--- Proto1 innovation/pages/Upload.py
+++ Proto1 innovation/pages/Upload.py
@@ -20,7 +20,8 @@ def main():
             st.image(image, caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
             st.text_input("Name", value="", max_chars=45, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
             st.text_input("Primary contact details", value="", max_chars=45, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
-            st.text_input("Secondary contact details", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")       st.text_input("Country and address of digsite/find", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+            st.text_input("Secondary contact details", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")       
+            st.text_input("Country and address of digsite/find", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
             st.text_input("Company/organisation", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
             end = st.button("Submit", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")  
         
@@ -28,6 +29,7 @@ def main():
         
 
 main()
+
 
 
 
